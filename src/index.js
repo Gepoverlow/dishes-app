438d281e5c14624d1b6098c0f20b64389fc70c0f
Gepoverlow <gepeoverload@gmail.com>
2022-01-27 18:02:53 +0100

    added some basic logic module to start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./css/style.css";
+import arrayOfRecipes from "./modules/logic.js";
 
-console.log("hi");
+console.log(arrayOfRecipes);
---
+++ src/modules/logic.js
@@ -0,0 +1,55 @@
+let arrayOfRecipes = [
+  {
+    name: "Arroz con Pollo",
+    ingredients: ["Arroz", "Pollo"],
+  },
+  {
+    name: "Fideos con Salsa",
+    ingredients: ["Fideos", "Salsa"],
+  },
+  {
+    name: "Hamburgesas",
+    ingredients: ["Carne Picada", "Pan", "Cebolla"],
+  },
+  {
+    name: "Ensalada de primavera",
+    ingredients: ["Fideos", "Mayonesa", "Atun", "Choclo"],
+  },
+];
+
+let totalIngredients = [
+  "Arroz",
+  "Pollo",
+  "Fideos",
+  "Salsa",
+  "Carne Picada",
+  "Pan",
+  "Cebolla",
+  "Salsa de Tomate",
+  "Atun",
+  "Choclo",
+  "Mayonesa",
+  "Leche",
+  "Huevos",
+  "Crema",
+  "Queso Rayado",
+  "Pepino",
+  "Lechuga",
+  "Jamon",
+  "Chorizo",
+  "Queso Feta",
+  "Cebollin",
+  "Tomate",
+  "Falafel",
+  "Manteca",
+  "Broccoli",
+  "Avocado",
+  "Limon",
+  "Zanahoria",
+  "Patata",
+  "Lentejas",
+  "Ajo",
+];
+let currentIngedients = [];
+
+module.exports = arrayOfRecipes;
